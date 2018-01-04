1cd268871f18a239fcbbf42f9fd66a663592f202
phyxolog <yzhivaga@gmail.com>
2018-01-04 12:47:54 +0200

    [oO] Some fixes

--- extractor.py
+++ extractor.py
@@ -22,32 +22,38 @@ class Extractor:
       os.makedirs(self.extract_dir)
 
   def __gen_file_name__(self, i, result):
-    return ".".join([os.path.join(self.extract_dir, str(i + 1).zfill(10)), ftypes.type_to_ext(result["type"])])    
+    return ".".join([os.path.join(self.extract_dir, str(i).zfill(10)), ftypes.type_to_ext(result["type"])])    
 
-  def extract(self, offset, size, type, out_file_name):
+  def __extract__(self, offset, size, type, out_file_name):
     logging.debug("Extracting {0} to {1}, {2}".format(type.upper(), out_file_name, helper.humn_size(size)))
 
-    processed_bytes = 0
+    read_bytes = 0
     buffer_size = 131072 # 128kB
     buffer = bytearray(buffer_size)
     file = io.open(out_file_name, "wb")
     self.file.seek(offset)
 
-    while processed_bytes <= size:
-      if size < buffer_size:
-        buffer_size = size
+    if size < buffer_size:
+      buffer_size = size
+      buffer = bytearray(buffer_size)
+
+    while read_bytes < size:
+      if read_bytes + buffer_size > size:
+        buffer_size = size - read_bytes
         buffer = bytearray(buffer_size)
 
       self.file.readinto(buffer)
       file.write(buffer)
-      processed_bytes += buffer_size
+      read_bytes += buffer_size
+
+    if read_bytes != size:
+      logging.error("Error extracting {0}, size: {1}, at {2}".format(type.upper(), helper.humn_size(size), hex(offset).upper()))
 
     file.close()
 
   def run(self):
-    i = 0
+    i = 1
     for result in self.results:
-      out_file_name = self.__gen_file_name__(i, result)
-      self.extract(result["offset"], result["size"], result["type"], out_file_name)
+      self.__extract__(result["offset"], result["size"], result["type"], self.__gen_file_name__(i, result))
       i += 1
     self.file.close()
